11149afc53ba15ab57b64610eb72d8e162507f07
winsa24 <67416652+winsa24@users.noreply.github.com>
2021-01-24 20:31:30 +0100

    submit

---
+++ VRParkour/Assets/Scripts/LocomotionTechnique.cs
@@ -0,0 +1,225 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocomotionTechnique : MonoBehaviour
+{
+    // Please implement your locomotion technique in this script. 
+    public OVRInput.Controller leftController;
+    public OVRInput.Controller rightController;
+    [Range(0, 10)] public float translationGain = 0.5f;
+    public GameObject hmd;
+    [SerializeField] private float leftTriggerValue;    
+    [SerializeField] private float rightTriggerValue;
+    [SerializeField] private Vector3 startPos;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private bool isIndexTriggerDown;
+
+    public float speed = 0f;
+    public float maxSpeed = 8f;
+    public float rotateSpeed = 1.5f;
+    public float maxrotateSpeed = 3f;
+
+    public float rotation = 0f;
+    private Quaternion mRotation;
+
+    private Vector3 initpositionL;
+    private Vector3 initpositionR;
+
+    public GameObject levelText;
+    private int Lflag = 0;
+    private int Rflag = 0;
+
+    /////////////////////////////////////////////////////////
+    // These are for the game mechanism.
+    public ParkourCounter parkourCounter;
+    public string stage;
+    
+    void Start()
+    {
+        levelText.SetActive(true);
+    }
+
+    void Update()
+    {
+        ////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Please implement your LOCOMOTION TECHNIQUE in this script :D.
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            initpositionR = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+            Rflag += 1;
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Three))
+        {
+            initpositionL = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+            Lflag += 1;
+        }
+        if(Rflag >=1 && Lflag <= 1)
+        {
+            levelText.SetActive(false);
+        }
+        
+
+        //speed up
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            speed -= 0.5f;
+        }
+        //speed down
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            speed += 0.5f;
+        }
+        if(speed > maxSpeed)
+        {
+            speed = maxSpeed;
+        }
+        if(speed < 0)
+        {
+            speed = 0;
+        }
+        
+
+        //rotate speed up
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger))
+        {            
+            rotateSpeed -= 0.1f;
+        }
+        //speed down
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        {
+            rotateSpeed += 0.1f;
+        }
+        if (rotateSpeed > maxrotateSpeed)
+        {
+            rotateSpeed = maxrotateSpeed;
+        }
+        if (rotateSpeed < 1)
+        {
+            rotateSpeed = 1f;
+        }
+
+        Vector3 movement = this.transform.TransformDirection(0, 0, 0);
+
+        Vector3 currentpositionL = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+        Vector3 currentpositionR = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+
+        movement.z += speed;
+
+
+
+        //left
+        if ((currentpositionL.y > initpositionL.y) && (currentpositionR.y < initpositionR.y))
+        {
+            //rotation += rotateSpeed;
+            rotation += ((currentpositionL.y - initpositionL.y) - (currentpositionR.y - initpositionR.y)) / 2 * rotateSpeed;
+
+        }
+        //right
+        if ((currentpositionL.y < initpositionL.y) && (currentpositionR.y > initpositionR.y))
+        {
+            //rotation -= rotateSpeed;
+            rotation -= (- (currentpositionL.y - initpositionL.y) + (currentpositionR.y - initpositionR.y)) / 2 * rotateSpeed;
+
+        }
+
+        ////left-shift
+        //if (OVRInput.GetDown(OVRInput.Button.Four))
+        //{
+        //    rotation += 40;
+        //}
+        ////right-shift
+        //if (OVRInput.GetDown(OVRInput.Button.Two))
+        //{
+        //    rotation -= 40;
+        //}
+
+        //work
+        this.transform.position += movement.magnitude * hmd.transform.forward.normalized * 0.08f;
+        //instead up&down
+        //this.transform.position += movement.magnitude * hmd.transform.up * 0.1f;
+        //Debug.DrawLine (Vector3.zero, new Vector3 (0, 1, 0), Color.red);
+
+        mRotation = Quaternion.Euler(0, rotation, 0);
+        this.transform.rotation = mRotation;
+
+
+
+        //leftTriggerValue = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, leftController);
+        //rightTriggerValue = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, rightController);
+
+        //if (leftTriggerValue > 0.95f && rightTriggerValue > 0.95f)
+        //{
+        //    if (!isIndexTriggerDown)
+        //    {
+        //        isIndexTriggerDown = true;
+        //        startPos = (OVRInput.GetLocalControllerPosition(rightController) + OVRInput.GetLocalControllerPosition(rightController)) / 2;
+        //    }
+        //    offset = hmd.transform.forward.normalized *
+        //            ((OVRInput.GetLocalControllerPosition(leftController) - startPos) +
+        //             (OVRInput.GetLocalControllerPosition(rightController) - startPos)).magnitude;
+        //    Debug.DrawRay(startPos, offset, Color.red, 0.2f);
+        //}
+        //else if (leftTriggerValue > 0.95f && rightTriggerValue < 0.95f)
+        //{
+        //    if (!isIndexTriggerDown)
+        //    {
+        //        isIndexTriggerDown = true;
+        //        startPos = OVRInput.GetLocalControllerPosition(leftController);
+        //    }
+        //    offset = hmd.transform.forward.normalized *
+        //             (OVRInput.GetLocalControllerPosition(leftController) - startPos).magnitude;
+        //    Debug.DrawRay(startPos, offset, Color.red, 0.2f);
+        //}
+        //else if (leftTriggerValue < 0.95f && rightTriggerValue > 0.95f)
+        //{
+        //    if (!isIndexTriggerDown)
+        //    {
+        //        isIndexTriggerDown = true;
+        //        startPos = OVRInput.GetLocalControllerPosition(rightController);
+        //    }
+        //    offset = hmd.transform.forward.normalized *
+        //             (OVRInput.GetLocalControllerPosition(rightController) - startPos).magnitude;
+        //    Debug.DrawRay(startPos, offset, Color.red, 0.2f);
+        //}
+        //else
+        //{
+        //    if (isIndexTriggerDown)
+        //    {
+        //        isIndexTriggerDown = false;
+        //        offset = Vector3.zero;
+        //    }
+        //}
+        //this.transform.position = this.transform.position + (offset) * translationGain;
+
+
+        ////////////////////////////////////////////////////////////////////////////////
+        // These are for the game mechanism.
+        if (OVRInput.Get(OVRInput.Button.Two) || OVRInput.Get(OVRInput.Button.Four))
+        {
+            if (parkourCounter.parkourStart)
+            {
+                this.transform.position = parkourCounter.currentRespawnPos;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        // These are for the game mechanism.
+        if (other.CompareTag("banner"))
+        {
+            stage = other.gameObject.name;
+            parkourCounter.isStageChange = true;
+        }
+        else if (other.CompareTag("coin"))
+        {
+            parkourCounter.coinCount += 1;
+            this.GetComponent<AudioSource>().Play();
+            other.gameObject.SetActive(false);
+        }
+        // These are for the game mechanism.
+
+    }
+}
---
+++ VRParkour/Assets/Scripts/ParkourCounter.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ParkourCounter : MonoBehaviour
+{
+    public LocomotionTechnique locomotionTech;
+    public bool isStageChange;
+    // banners
+    public GameObject startBanner;
+    public GameObject firstBanner;
+    public GameObject secondBanner;
+    public GameObject finalBanner;
+    // coins
+    public GameObject firstCoins;
+    public GameObject secondCoins;
+    public GameObject finalCoins;
+    // respawn points
+    public Transform start2FirstRespawn;
+    public Transform first2SecondRespawn;
+    public Transform second2FinalRespawn;
+    public Vector3 currentRespawnPos;
+
+    public float timeCounter;
+    private float part1Time;
+    private float part2Time;
+    private float part3Time;
+    public int coinCount;
+    
+    private int part1Count; // 17
+    private int part2Count; // 33
+    private int part3Count; // 24
+    public bool parkourStart;
+
+    public TMP_Text timeText;
+    public TMP_Text coinText;
+    public TMP_Text recordText;
+    public GameObject timeTextGO;
+    public GameObject coinTextGO;
+    public GameObject recordTextGO;
+    public GameObject endTextGO;
+    public AudioSource backgroundMusic;
+    public AudioSource endSoundEffect;
+
+    public GameObject welcomeText;
+    public GameObject FightingText;
+    public GameObject AllezText;
+    public GameObject IppImage;
+
+    void Start()
+    {
+        coinCount = 0;
+        timeCounter = 0.0f;
+        firstBanner.SetActive(false);
+        secondBanner.SetActive(false);
+        finalBanner.SetActive(false);
+        firstCoins.SetActive(false);
+        secondCoins.SetActive(false);
+        finalCoins.SetActive(false);
+        parkourStart = false;
+        endTextGO.SetActive(false);
+
+        welcomeText.SetActive(false);
+        FightingText.SetActive(false);
+        AllezText.SetActive(false);
+        IppImage.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (isStageChange)
+        {
+            isStageChange = false;
+            if (locomotionTech.stage == startBanner.name)
+            {
+                parkourStart = true;
+                startBanner.SetActive(false);
+                firstBanner.SetActive(true);
+                firstCoins.SetActive(true);
+                currentRespawnPos = start2FirstRespawn.position;
+
+                welcomeText.SetActive(true);
+                FightingText.SetActive(false);
+                AllezText.SetActive(false);
+            }
+            else if (locomotionTech.stage == firstBanner.name)
+            {
+                firstBanner.SetActive(false);
+                firstCoins.SetActive(false);
+                secondBanner.SetActive(true);
+                secondCoins.SetActive(true);
+                part1Time = timeCounter;
+                part1Count = coinCount;
+                currentRespawnPos = first2SecondRespawn.position;
+                UpdateRecordText(1, part1Time, part1Count, 17);
+
+                welcomeText.SetActive(false);
+                FightingText.SetActive(true);
+                AllezText.SetActive(false);
+            }
+            else if (locomotionTech.stage == secondBanner.name)
+            {
+                secondBanner.SetActive(false);
+                secondCoins.SetActive(false);
+                finalBanner.SetActive(true);
+                finalCoins.SetActive(true);
+                part2Time = timeCounter - part1Time;
+                part2Count = coinCount - part1Count;
+                currentRespawnPos = second2FinalRespawn.position;
+                UpdateRecordText(2, part2Time, part2Count, 33);
+
+                welcomeText.SetActive(false);
+                FightingText.SetActive(false);
+                AllezText.SetActive(true);
+            }
+            else if (locomotionTech.stage == finalBanner.name)
+            {
+                parkourStart = false;
+                finalCoins.SetActive(false);
+                part3Time = timeCounter - (part1Time + part2Time);
+                part3Count = coinCount - (part1Count + part2Count);
+                UpdateRecordText(3, part3Time, part3Count, 24);
+                timeTextGO.SetActive(false);
+                coinTextGO.SetActive(false);
+                recordTextGO.SetActive(false);
+                endTextGO.SetActive(true);
+                endTextGO.GetComponent<TMP_Text>().text = "Year Finished!\n" + recordText.text +
+                    "\ntotal: " + timeCounter.ToString("F2") + ", " + coinCount.ToString() + "/74";
+                Debug.Log(endTextGO.GetComponent<TMP_Text>().text);
+                endSoundEffect.Play();
+
+                welcomeText.SetActive(false);
+                FightingText.SetActive(false);
+                AllezText.SetActive(false);
+                IppImage.SetActive(true);
+            }
+        }
+
+        if (parkourStart)
+        {
+            timeCounter += Time.deltaTime;
+            timeText.text = "time: " + timeCounter.ToString("F2");
+            coinText.text = "coins: " + coinCount.ToString();
+        }       
+    }
+
+    void UpdateRecordText(int part, float time, int coinsCount, int coinsInPart)
+    {
+        string newRecords = "semester" + part.ToString() + ": " + time.ToString("F2") + ", " + coinsCount + "/" + coinsInPart;
+        recordText.text = recordText.text + "\n" + newRecords;
+    }
+}
